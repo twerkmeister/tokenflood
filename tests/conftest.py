bf8d296738d0adcadb779252e1bd8c0e0cac80d7
Thomas Werkmeister <thomas@werkmeister.me>
2025-10-15 10:33:26 +0200

    initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import os
+import pytest
+
+@pytest.fixture(scope="session")
+def test_folder() -> str:
+    folder = os.path.dirname(__file__)
+    assert folder.endswith(os.path.join("tokenflood", "tests"))
+    assert os.path.exists(folder)
+    return folder
