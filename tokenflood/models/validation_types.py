384fe12cb5a9f1cd402fb7e1e2c23aa55ad152e8
Thomas Werkmeister <thomas@werkmeister.me>
2025-11-14 12:29:49 +0100

    removed unused validator and types

--- tokenflood/models/validation_types.py
+++ tokenflood/models/validation_types.py
@@ -4,7 +4,6 @@ from pydantic import AfterValidator, Field, StringConstraints
 
 from tokenflood.models.validators import (
     all_non_empty_strings,
-    all_positive,
     all_strictly_positive,
     at_least_size,
     non_empty_list,
@@ -53,8 +52,3 @@ PositiveUniqueFloats = Annotated[
     AfterValidator(unique_elements),
     AfterValidator(all_strictly_positive),
 ]
-NonNegativeIntegers = Annotated[
-    Tuple[int, ...], AfterValidator(non_empty_list), AfterValidator(all_positive)
-]
-
-NonNegativeIntegersOrEmpty = Annotated[Tuple[int, ...], AfterValidator(all_positive)]
